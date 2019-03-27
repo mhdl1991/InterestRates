fd22a03e8d9ba8b6a7c291eca28e09cff394b0a2
M. Ali Khan <45436022+mhdl1991@users.noreply.github.com>
2019-03-27 17:59:45 +0500

    Update getGoldBullion.py
    Code for this is now much simpler. Uses requests library instead of urllib.requests
    
    I will see if I can do this with the other scripts

--- getGoldBullion.py
+++ getGoldBullion.py
@@ -6,40 +6,27 @@ Created on Mon Mar 25 16:40:25 2019
 """
 
 #Get Gold Bullion Rates from Bullion-Rates.com
-from urllib.request import urlopen, Request
+import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-
-#USER AGENT STRING
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'}
-
-#PROXY SETTINGS
-#proxy = "[redacted]"
-
 
 Years = ["2015","2016","2017","2018"]
 Months = ["01","02","03","04","05","06","07","08","09","10","11","12"]
 baseURL = "https://www.bullion-rates.com/gold/PKR/%s-%s-history.htm"
 URLList = [baseURL%(Year,Month) for Year in Years for Month in Months]
 
-
 BullionRatesData = []
-BullionRatesDataFrame = pd.DataFrame()
-
 
 for URL in URLList:
     try:
         #Access Bullion-rates website and pull the tables from it using BeautifulSoup
-        req = Request(URL, headers = headers)
-        #req.set_proxy(proxy, 'http')
-        site = urlopen(req)
-        print(type(site))
-        raw_html = site.read()
-        html = BeautifulSoup(raw_html, 'html.parser')
-        table = html.find("table", attrs = {'id': 'drDGrid'})
-        for row in table.findAll("tr"): 
+        print(URL)
+        website = requests.get(URL, verify = False)
+        soup = BeautifulSoup(website.text,'html.parser')
+        table = soup.find("table", attrs = {'id': 'dtDGrid'})
+        for row in table.find_all("tr"): 
             #go through each row in the table
-            cols = row.findAll("td")
+            cols = row.find_all("td")
             #The cells in each row are Date, Price/oz and Price/gm
             cols = [ele.text.strip() for ele in cols]
             BullionRatesData.append([ele for ele in cols if ele]) 
@@ -48,10 +35,15 @@ for URL in URLList:
     except Exception as e:
         print(str(e))
 
+BullionRatesData = [row for row in BullionRatesData if row]
 
+BullionRatesDataFrame = pd.DataFrame(BullionRatesData, columns=['Date', 'Price/oz', 'Price/gm'])
+#TIME TO CONVERT.
+BullionRatesDataFrame['Date'] = pd.to_datetime(BullionRatesDataFrame['Date'], format = '%d/%m/%y')
 
+BullionRatesDataFrame['Price/oz'] = BullionRatesDataFrame['Price/oz'].str.replace(',','').astype(float)
+BullionRatesDataFrame['Price/gm'] = BullionRatesDataFrame['Price/gm'].str.replace(',','').astype(float)
 
 
 BullionRatesDataFrame.to_csv("D:\\Python36_projects\\StateBankPakistan\\BullionData.csv", index = False)
-
 
