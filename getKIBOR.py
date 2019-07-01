04fa6e66a166065985705d095631d996b8b71530
M. Ali Khan <45436022+mhdl1991@users.noreply.github.com>
2019-07-01 17:31:03 +0500

    Update getKIBOR.py
    changes to regex

--- getKIBOR.py
+++ getKIBOR.py
@@ -12,10 +12,10 @@ import matplotlib.pyplot as plt
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'}
 
 #PROXY SETTINGS
-#proxy = "[redacted]"
+proxy = "[redacted]"
 
 #MONTHS AND YEARS
-years = ["2018","2017","2016","2015"]#["2018","2017","2016","2015","2014","2013","2012"]
+years = ["2018","2017","2016","2015"] #["2018","2017","2016","2015","2014","2013","2012"]
 months = ["Dec","Nov","Oct","Sep","Aug","Jul","Jun","May","Apr","Mar","Feb","Jan"]
 
 #DOWNLOAD DAILY KIBOR RATES FOR EVERY DAY OF EVERY MONTH OF THE SELECTED RANGE OF YEARS
@@ -26,7 +26,7 @@ for year in years:
         print(URL)
         try:
             req = Request(URL, headers = headers)
-            #req.set_proxy(proxy, 'http')
+            req.set_proxy(proxy, 'http')
             site = urlopen(req)
             #Use BeautifulSoup?
             raw_html = site.read()
@@ -34,7 +34,7 @@ for year in years:
             for a in html.select('a'):
                 download_URL = ""
                 #If the link is a download link for a PDF
-                if re.match(r"kibor-\d\d-[a-z][a-z][a-z]-\d\d\.pdf",a['href'],flags=re.IGNORECASE):
+                if re.match(r"kibor-\d\d-[a-z][a-z][a-z]-(\d\d|\d\d\d\d)\.pdf",a['href'],flags=re.IGNORECASE):
                     download_URL = "http://www.sbp.org.pk/ecodata/kibor/" + year +"/" + month + "/" + str(a['href'])
                     print(download_URL)
                 elif re.match(r"http://www\.sbp\.org\.pk/ecodata/kibor/\d\d\d\d/[a-z][a-z][a-z]/kibor-\d\d-[a-z][a-z][a-z]-\d\d\.pdf", a['href'],flags=re.IGNORECASE):
@@ -43,33 +43,33 @@ for year in years:
                     print(download_URL)
                 if download_URL:
                     PDFs_List.append(download_URL)
-
+                     
         except Exception as e:
             print(str(e))
-
+    
 #SAVE THE LIST OF URLS FOR LATER 
 if PDFs_List:
-    with open('C:\\StateBankPakistanProject\\PDFdumpFolder\\pdf_list.txt','w') as f:
+    with open('pdf_list.txt','w') as f:
         for item in PDFs_List:
             f.write("{}\n".format(item))
 
 #DOWNLOAD EVERYTHNG IN THE LIST:
-PDFdumpPath = "C:\\StateBankPakistanProject\\PDFdumpFolder\\"
+PDFdumpPath = "D:\\Python36_projects\\StateBankPakistan\\PDFdumpFolder\\"
 if PDFs_List:
     for URL in PDFs_List:
         print(URL)
         try:
             req = Request(URL, headers = headers)
-            #req.set_proxy(proxy, 'http')
+            req.set_proxy(proxy, 'http')
             file = urlopen(req)
             file = file.read()
             filename = PDFdumpPath + URL.split('/')[-1]
             with open(filename,'wb') as f:
                 f.write(file)
-
+            
         except Exception as e:
             print(str(e))
-
+            
 #ONCE YOU HAVE ALL THE PDFS AND HAVE DOWNLOADED THEM IT'S TIME TO PROCESS THEM
 #ALL FILENAMES ARE IN THE FORM Kibor-DD-MMM-YY.pdf 
 #USE STRING SPLITTING STUFF TO EXTRACT THE DATE FROM THAT FILENAME
@@ -89,13 +89,13 @@ KIBORdf = KIBORdf.sort_values(by='DATE')
 KIBORdf = KIBORdf.reset_index()
 KIBORdf = KIBORdf.drop("index", axis = 1)
 KIBORdf.rename(columns = {"Tenor":"TENOR"}, inplace = True)
-KIBORdf.to_csv("C:\\StateBankPakistanProject\\DatadumpFolder\\KIBORdata.csv")
+KIBORdf.to_csv("D:\\Python36_projects\\StateBankPakistan\\KIBORdata.csv", index = False)
 
-#Try making a chart?
-KIBOR_1yr = KIBORdf['TENOR'] == '1 - Year'
-OneYear = KIBORdf[KIBOR_1yr]
 
-ax = plt.gca()
-OneYear.plot(kind='line',x='DATE',y='BID', ax = ax)
-OneYear.plot(kind='line',x='DATE',y='OFFER', ax = ax)
-plt.show()
+
+#MAYBE ADD A STEP HERE TO LOAD FROM CSV?
+KIBORdf = pd.read_csv("D:\\Python36_projects\\StateBankPakistan\\KIBORdata.csv",date_parser = "DATE")
+KIBORdf['DATE'] = pd.to_datetime(KIBORdf['DATE'], format = '%Y-%m-%d')
+KIBORdf = KIBORdf.drop('Unnamed: 0', axis = 1)
+KIBORdf.index = KIBORdf['DATE']
+KIBORdf = KIBORdf.drop('DATE', axis = 1)
